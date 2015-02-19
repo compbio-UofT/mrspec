3a6028bd9562388706c2b67945b161fc91cd25ee
FilipM <filip.miscevic@gmail.com>
2015-02-19 13:25:03 -0500

    Now uses correct table to assign DatabaseID.

--- update_database.py
+++ update_database.py
@@ -4,7 +4,7 @@ from create_database import *
 con,cur=establish_connection(sys.argv)
 
 def update_database_ID(table):
-    if not column_exists('standard', 'DatabaseID'):
+    if not column_exists(table, 'DatabaseID'):
         cur.execute("alter table {} add column DatabaseID BIGINT(21)".format(table))
     for result in cur.execute("set @i=0;update {0} as t inner join (select ID,Scan_ID,@i:=@i+1 as num from (select ID,Scan_ID from {0} group by ID order by Scan_ID) t2) t1 on t.ID=t1.ID set DatabaseID=num;".format(table),multi=True): pass
     con.commit()
