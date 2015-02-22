d8ac9400e4ba31eeef95f1d83fcd718dddead6a0
FilipM <filip.miscevic@gmail.com>
2015-02-22 01:21:52 -0500

    Created DatabaseConnection class.
    More elegant handling of connections.

--- connection.py
+++ connection.py
@@ -8,43 +8,57 @@ def is_run_from_commandline():
         return True
     return False
 
-def establish_connection(args, silent=False):
-         
-    if inspect.getouterframes(inspect.currentframe())[1][1] == main.__file__:
-        #Initialize connection to MySQL database
-        #usage: python query.py <user> <password> <port>
-        try:
-            con = m.connect(user=args[1], password=args[2],
-                                    database='mrspec', port=args[3])
-            cur = con.cursor()
-            return con, cur
-        except IndexError as e:
-            if not silent: sys.stderr.write('Incorrect commandline usage.\nUsage: python <filename> <user> <password> <port>')              
-    else:
-        #get credentials from file or user input
-        try:
-            with open("credentials.txt", 'r') as c:
-                user = c.next()
-                password = c.next()
-        except IOError as e:
-            #stdin.readline used here for compatibility with Python 2.7 and 3.x
-            print('User:')
-            user = sys.stdin.readline()
-            print('Password:')
-            password = sys.stdin.readline()
+class DatabaseConnection(object):
     
-        if not silent: print('Credentials loaded successfully.')
+    def __init__(self, sysargs, silent=False):
+        self.silent = silent            
+        self.con, self.cur = self._establish_connection(sysargs)
     
-        con = m.connect(
-                user=user,
-                password=password,
-                database='mrspec')
-    
-        cur = con.cursor()
-    if not silent: print('Connection to database successful.\n')
-    return con,cur
+    def __enter__(self):
+        return self.con, self.cur
         
+    def _establish_connection(self, args):
+
+        if inspect.getouterframes(inspect.currentframe())[1][1] == main.__file__:
+            #Initialize connection to MySQL database
+            #usage: python query.py <user> <password> <port>
+            try:
+                con = m.connect(user=args[1], password=args[2],
+                                        database='mrspec', port=args[3])
+                cur = con.cursor()
+                return con, cur
+            except IndexError as e:
+                if not self.silent: sys.stderr.write('Incorrect commandline usage.\nUsage: python <filename> <user> <password> <port>')              
+        else:
+            #get credentials from file or user input
+            try:
+                with open("credentials.txt", 'r') as c:
+                    user = c.next()
+                    password = c.next()
+            except IOError as e:
+                #stdin.readline used here for compatibility with Python 2.7 and 3.x
+                print('User:')
+                user = sys.stdin.readline()
+                print('Password:')
+                password = sys.stdin.readline()
+        
+            if not self.silent: print('Credentials loaded successfully.')
+        
+            con = m.connect(
+                    user=user,
+                    password=password,
+                    database='mrspec')
+        
+            cur = con.cursor()
+        if not self.silent: print('Connection to database successful.\n')
+        return con,cur
+    
+    def __exit__(self, type, value, traceback):
+        self.con.close()
+        if not self.silent: print('\nConnection to database closed.')
+                
 if __name__ == "__main__":
-    con,cur = establish_connection('', False)
     print(is_run_from_commandline())
-    con.close()
+    
+    with DatabaseConnection(sys.argv) as (con, cur):
+        print('test')
